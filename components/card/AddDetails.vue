036f91a77e4246f2c8a7c0dc1d4ae1e7c34bdea7
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2024-08-07 23:36:37 +0800

    Fix: typo for last section of the card

--- components/card/AddDetails.vue
+++ components/card/AddDetails.vue
@@ -66,7 +66,7 @@ import { UserIcon, BriefcaseIcon, UserGroupIcon, BuildingOffice2Icon, ShieldChec
     </div>
 
     <div class="mt-5">
-        <CardSubtitle forId="add-details" text="Business" />
+        <CardSubtitle forId="add-details" text="Payment" />
         <div class="mt-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5">
             <IconTitleIconify icon="ic:baseline-paypal" foreground="gray" background="gray" text="Paypal" />
             <IconTitleIconify icon="bxl:venmo" foreground="gray" background="gray" text="Venmo" />
